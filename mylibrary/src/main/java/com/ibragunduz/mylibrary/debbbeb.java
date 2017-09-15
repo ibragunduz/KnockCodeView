23b25c6513ac2c6ea326fcfb1135c7b1b6e98d06
ibrahim gunduz <ibrahim@ibrahim-MBP.home>
2017-09-15 17:58:31 +0300

    first commit

---
+++ mylibrary/src/main/java/com/ibragunduz/mylibrary/debbbeb.java
@@ -0,0 +1,13 @@
+package com.ibragunduz.mylibrary;
+
+/**
+ * Created by ibrahim on 15.09.2017.
+ */
+
+public class debbbeb {
+    String abv="selam";
+
+    public String getAbv() {
+        return abv;
+    }
+}
